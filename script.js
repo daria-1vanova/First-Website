092fd2aca93af4328a85eb5957bca3d4db477a1c
daria-1vanova <dashka.id@gmail.com>
2023-12-01 14:54:37 +0200

    conditional statement

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ let isIsland = false;
 console.log(isIsland);
 let countryDescription = `${country} is located in ${continent}, ${population} million people live there.`;
 console.log(countryDescription);
+
+
+let weightSarah = 65;
+let weightBob = 93;
+let heightSarah = 1.75;
+let heightBob = 1.89;
+let sarahBMI = weightSarah / Math.pow(heightSarah, 2);
+let bobBMI = weightBob / Math.pow(heightBob, 2);
+if (sarahBMI < 18.5) {
+    console.log('У Сары недостаточный вес');
+} 
+else if (sarahBMI >= 18.5 && sarahBMI <= 25) {
+    console.log('У Сары нормальный вес');
+}
+if (bobBMI < 18.5 || bobBMI > 25) {
+    console.log('У Боба недостаточный или избыточный вес');
+} else {
+    console.log('У Боба нормальный вес');
+}
